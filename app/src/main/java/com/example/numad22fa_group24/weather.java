0f513f2dee9d0bf12519fcff61a4a12ada3918c8
zhijie <yang.zhij@northeastern.edu>
2022-10-20 22:04:30 -0700

    init commit

--- app/src/main/java/com/example/numad22fa_group24/weather.java
+++ app/src/main/java/com/example/numad22fa_group24/weather.java
@@ -3,6 +3,8 @@ package com.example.numad22fa_group24;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class weather extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class weather extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+
+        Button check = findViewById(R.id.checkButton);
+        check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
