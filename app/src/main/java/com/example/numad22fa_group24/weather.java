4a3a00c374c385e7db1550009743e5c8fa119b96
zhijie <yang.zhij@northeastern.edu>
2022-10-21 09:53:01 -0700

    update weather activity

--- app/src/main/java/com/example/numad22fa_group24/weather.java
+++ app/src/main/java/com/example/numad22fa_group24/weather.java
@@ -1,24 +1,37 @@
 package com.example.numad22fa_group24;
 
+import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.net.URL;
 
 public class weather extends AppCompatActivity {
+
+    EditText cityName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
+        cityName = findViewById(R.id.cityName);
         Button check = findViewById(R.id.checkButton);
         check.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                checkWeather();
             }
         });
+    }
+
+    private void checkWeather() {
+//        String city = cityName.getText().toString();
+//        URL urlLocation = new URL("http://localhost:3000/weather/");
     }
 }
