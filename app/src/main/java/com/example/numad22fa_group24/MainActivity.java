7fa071c958f4596b693af1cc5a817a9f2cd4adc3
zhijie <yang.zhij@northeastern.edu>
2022-10-20 21:58:13 -0700

    init commit

--- app/src/main/java/com/example/numad22fa_group24/MainActivity.java
+++ app/src/main/java/com/example/numad22fa_group24/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.numad22fa_group24;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button weather = findViewById(R.id.weatherButton);
+        weather.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                weatherActivity();
+            }
+        });
+    }
+
+    private void weatherActivity() {
+        Intent intent = new Intent(this, weather.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/numad22fa_group24/weather.java
@@ -0,0 +1,14 @@
+package com.example.numad22fa_group24;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class weather extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+}
