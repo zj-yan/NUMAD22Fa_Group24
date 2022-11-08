3750345d2770db975e4115b2dd2fa14ed6861710
zhijie <yang.zhij@northeastern.edu>
2022-11-07 17:31:21 -0800

    add notifications

---
+++ app/src/main/java/com/example/numad22fa_group24/notification/MyFirebaseMessagingService.java
@@ -0,0 +1,80 @@
+package com.example.numad22fa_group24.notification;
+
+import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+import com.example.numad22fa_group24.R;
+import com.example.numad22fa_group24.activities.LoginActivity;
+import com.example.numad22fa_group24.util.Utils;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String CHANNEL_ID = "com.example.numad22fa_group24.stick_it";
+    private static final String CHANNEL_NAME = "stick_it";
+
+    @Override
+    public void onNewToken(@NonNull String token) {
+        super.onNewToken(token);
+        Utils.updateDeviceToken(this, token);
+    }
+
+    @SuppressLint("UnspecifiedImmutableFlag")
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        String title = remoteMessage.getData().get("title");
+        String message = remoteMessage.getData().get("body");
+        String image = remoteMessage.getData().get("img");
+        Intent intentChat = new Intent(this, LoginActivity.class);
+
+        PendingIntent pendingIntent = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
+            pendingIntent = PendingIntent.getActivity(this, 0, intentChat, PendingIntent.FLAG_MUTABLE);
+        } else {
+            pendingIntent = PendingIntent.getActivity(this, 0, intentChat, PendingIntent.FLAG_ONE_SHOT);
+        }
+
+        final NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        final NotificationCompat.Builder notificationBuilder;
+
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
+            channel.setDescription("Chat App notifications");
+            notificationManager.createNotificationChannel(channel);
+            notificationBuilder = new NotificationCompat.Builder(this, CHANNEL_ID);
+        } else {
+            notificationBuilder = new NotificationCompat.Builder(this);
+        }
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), Utils.getImgId(image));
+        notificationBuilder
+                .setContentTitle(title)
+                .setContentText(message)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent)
+                .setSmallIcon(R.drawable.ic_account)
+                .setLargeIcon(bitmap)
+                .setAutoCancel(true);
+
+        notificationManager.notify(999, notificationBuilder.build());
+    }
+
+
+}
---
+++ app/src/main/java/com/example/numad22fa_group24/notification/Token.java
@@ -0,0 +1,17 @@
+package com.example.numad22fa_group24.notification;
+
+public class Token {
+    private String token;
+
+    public Token(String token) {
+        this.token = token;
+    }
+
+    public Token() {
+
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
