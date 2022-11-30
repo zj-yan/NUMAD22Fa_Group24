9c93f3d5c798daabea916b384d566ec1f8480c6e
Shiyan Chen <ch592127418@gmail.com>
2022-11-30 15:00:32 -0800

    built project framework

---
+++ app/src/main/java/com/example/numad22fa_group24/project/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.example.numad22fa_group24.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.numad22fa_group24.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat2);
+    }
+}
---
+++ app/src/main/java/com/example/numad22fa_group24/project/WorryActivity.java
@@ -0,0 +1,16 @@
+package com.example.numad22fa_group24.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.numad22fa_group24.R;
+
+public class WorryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_worry);
+    }
+}
